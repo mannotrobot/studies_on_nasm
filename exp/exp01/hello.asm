69ddbb6a5720cf2dc0dc67c9f6a7060e11df462a
mannotrobot <boxforgit@gmail.com>
2023-08-25 23:42:23 +0300

    update .gitignore

--- exp/exp01/hello.asm
+++ exp/exp01/hello.asm
@@ -2,7 +2,8 @@
 global _start
 
 section .data
-    msg    db    "hEllo, world",0
+    msg    db    "Hello, world",0
+    NL     db     0xA
 section .bss
 section .text
 
@@ -11,6 +12,11 @@ _start: mov rax, 1         ; 1 = record
         mov rsi, msg       ; в регистр rsi msg
         mov     rdx, 12    ; в регистр rdx занести длину строки без 0
         syscall            ; вывести на экран;
+        mov     rax, 1
+        mov     rdi, 1
+        mov     rsi, NL
+        mov     rdx, 1
+        syscall
         mov     rax, 60    ; в регистр rax занести значение 60
         mov     rdi, 0     ; в регистр rdi занести значение 0
         syscall            ; выход

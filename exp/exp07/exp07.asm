8aaa76dad46b3860f01c6440d46f14b65d1525c7
mannotrobot <boxforgit@gmail.com>
2023-08-27 18:04:53 +0300

    memory research using ddd

---
+++ exp/exp07/exp07.asm
@@ -0,0 +1,37 @@
+; memory.asm
+section .data
+	bNum	db	123
+	wNum	dw	12345
+	warray	times	5 dw 0
+
+	dNum	dd	12345
+	qNum1	dq	12345
+	text1	db	"abc",0
+	qNum2	dq	3.141592654
+	text2	db	"cde",0
+section .bss
+	bvar	resb	1
+	dvar	resd	1
+	wvar	resw	10
+	qvar	resq	3
+section .text
+	global main
+main:
+	push	rbp
+	mov	rbp, rsp
+	lea	rax, [bNum] ; load addreses bNum in reg. rax
+	mov	rax, bNum   ; load addr. bNum in reg. rax
+	mov	rax, [bNum] ; load value bNum in reg. rax
+	mov	[bvar], rax ; load value rax := bvar
+	lea	rax, [bvar]
+	lea	rax, [wNum]
+	mov	rax, [wNum]
+	lea	rax, [text1]
+	mov	rax, text1
+	mov	rax, text1+1
+	lea	rax, [text1+1]
+	mov	rax, [text1]
+	mov	rax, [text1+1]
+	mov	rsp, rbp
+	pop	rbp
+	ret
